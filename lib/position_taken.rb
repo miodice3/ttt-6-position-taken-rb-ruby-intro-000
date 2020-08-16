9214f2688b40c5b6ac414814cf9fe78ce3168c04
Michael I <miodice3@gmail.com>
2020-08-16 14:13:04 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -5,12 +5,10 @@ board = [" ", "X", " ", " ", " ", " ", " ", " ", " "]
 def position_taken?(board, index) 
   if board[index] == " " || board[index] == "" || board[index] == nil
     false
-    puts "false"
   else board[index] == "X" || board[index] == "O"
     true
-    puts "true" 
   end
   
 end
 
-position_taken?(board, 0)
+#position_taken?(board, 0)
